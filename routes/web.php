ec3b844c2aa1564072c5ca8be936635acf4f5420
Nguyen Dang Khoa <khoand@vccvn.com>
2017-12-18 17:01:16 +0700

    + Update code

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::match(['get', 'post'],'/' ,'Home\HomeController@index')->name('home');
