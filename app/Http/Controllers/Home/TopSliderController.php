b7ff37a24ea13b1596dd8a08578d1fd9b1191c9a
Khoand <khoand@lifull-tech.vn>
2018-02-05 16:53:26 +0700

    + Update code

--- app/Http/Controllers/Home/TopSliderController.php
+++ app/Http/Controllers/Home/TopSliderController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Home;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Model\Product;
 
 class TopSliderController extends Controller
 {
-    //
+  public function index()
+  {
+    $products = Product::all();
+  }
 }
--- app/Model/product.php
+++ app/Model/product.php
@@ -4,14 +4,7 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
-class product extends Model
+class Product extends Model
 {
-  /**
-   * The table associated with the model.
-   *
-   * @var string
-   */
-  protected $table = 'products';
-
-  
+    //
 }
